075758d499d87b508e7a3caf1b09f6216c0fc61c
JuanM <jmalagon72@unisalle.edu.co>
2026-04-05 13:10:37 -0500

    clase faltante error

---
+++ Ejercicio 2/Museo/model/OtroObjeto.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+
+public class OtroObjeto extends Obra {
+
+    public OtroObjeto(String autor, String periodo, double valor,
+                      LocalDate fc, LocalDate fi) {
+        super(autor, periodo, valor, fc, fi);
+    }
+}
